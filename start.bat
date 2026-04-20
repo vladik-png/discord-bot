0d9d8cf1d2f1539dcef57c508f1e47e89bf217b5
vladik-png <vladsemotuk42@gmail.com>
2026-04-20 19:20:38 +0300

    start bat

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Start Lavalink...
+cd /d "D:\py"
+start "Lavalink Server" java -jar Lavalink.jar
+
+echo Waiting 5 seconds
+timeout /t 5 /nobreak >nul
+
+echo Start Bot...
+cd /d "D:\py"
+start "Bot" python main.py
